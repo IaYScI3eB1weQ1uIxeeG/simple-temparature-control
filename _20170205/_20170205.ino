d56f399acc52c707947baada564f1958c62312b1
IaYScI3eB1weQ1uIxeeG <rred@tutanota.com>
2017-02-05 21:03:20 +0000

    first commit

---
+++ _20170205/_20170205.ino
@@ -0,0 +1,47 @@
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is connected to the Arduino digital pin 2
+#define temp_sensor 2
+#define heater 7
+int temp;
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(temp_sensor);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+
+pinMode(heater, OUTPUT);
+  
+  // Start serial communication for debugging purposes
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+}
+
+
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  Serial.println(sensors.getTempCByIndex(0));
+  temp = sensors.getTempCByIndex(0); 
+  delay(10000);
+
+  if (temp <= 16){
+  
+  digitalWrite(heater, HIGH);
+
+  }
+  
+  if (temp >= 20){
+  
+  digitalWrite(heater, LOW);
+  }
+
+}
